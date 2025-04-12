9fdbd0597d16693397d6fe438e06e49514819b11
Sarah Chan <sjchan@umich.edu>
2025-04-12 00:03:04 -0400

    Updated plane tracking

--- identify_planes.py
+++ identify_planes.py
@@ -1,30 +1,62 @@
 import numpy as np
 import cv2
+import os
+import datetime
 
 daytime = True # need to figure out how to automate this
 nighttime = False
 
-def filter_contrail_contours(contours, min_length=100, min_aspect_ratio=5):
-    valid_contours = []
+# def filter_contrail_contours(contours, min_length=100, min_aspect_ratio=5):
+#     valid_contours = []
     
-    for cont in contours:
-        x, y, w, h = cv2.boundingRect(cont)
-        # aspect_ratio = max(w, h) / min(w, h + 1e-5)
-        aspect_ratio = w / (h + 1e-5)
-        length = cv2.arcLength(cont, closed=False)
+#     for cont in contours:
+#         x, y, w, h = cv2.boundingRect(cont)
+#         # aspect_ratio = max(w, h) / min(w, h + 1e-5)
+#         aspect_ratio = w / (h + 1e-5)
+#         length = cv2.arcLength(cont, closed=False)
         
-        if length > min_length and aspect_ratio > min_aspect_ratio:
-            valid_contours.append(cont)
+#         if length > min_length and aspect_ratio > min_aspect_ratio:
+#             valid_contours.append(cont)
 
-    return valid_contours
+#     return valid_contours
+
+def is_contrail_shape(contour, min_length=100, min_aspect_ratio=5, max_curvature=0.05):
+    if len(contour) < 5:
+        return False
+    
+    rect = cv2.minAreaRect(contour)
+    (x, y), (w, h), angle = rect
+    aspect_ratio = max(w, h) / (min(w, h) + 1e-5)
+    length = cv2.arcLength(contour, closed=False)
+    
+    if length < min_length or aspect_ratio < min_aspect_ratio:
+        return False
+
+    # Optionally: fit a line and compute how well points stay close to it
+    [vx, vy, x0, y0] = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
+    unit_vector = np.array([vx, vy])
+    
+    deviations = []
+    for pt in contour:
+        pt_vec = np.array([pt[0][0] - x0, pt[0][1] - y0])
+        projection = np.dot(pt_vec.T, unit_vector)
+        perpendicular = pt_vec - projection * unit_vector
+        deviations.append(np.linalg.norm(perpendicular))
+    
+    avg_deviation = np.mean(deviations)
+
+    # Curvier lines will have higher deviation from the best-fit line
+    return avg_deviation < max_curvature * length
+
 
 def identify_planes(image):
     if daytime:
         # identify using the contrail
         image_blurred = cv2.GaussianBlur(image, (3,3), 0)
-        edges = cv2.Canny(image_blurred, 5, 80)
+        edges = cv2.Canny(image_blurred, 50, 250)
         contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contrail_contours = filter_contrail_contours(contours)
+        # contrail_contours = filter_contrail_contours(contours)
+        contrail_contours = [cont for cont in contours if is_contrail_shape(cont)]
         cv2.drawContours(image, contrail_contours, -1, (0, 255, 0), 2)
 
         center_height = np.shape(image)[0] / 2
@@ -34,20 +66,26 @@ def identify_planes(image):
 
         for contrail_contour in contrail_contours:
             contrail_contour = np.array(contrail_contour)
-            candidates = [
+            # candidates = [
+            #     tuple(contrail_contour[contrail_contour[:, :, 0].argmin()][0]), # leftmost
+            #     tuple(contrail_contour[contrail_contour[:, :, 0].argmax()][0]), # rightmost
+            #     tuple(contrail_contour[contrail_contour[:, :, 1].argmin()][0]), # topmost
+            #     tuple(contrail_contour[contrail_contour[:, :, 1].argmax()][0]), # bottommost
+            # ]
+            contrail_end = [
                 tuple(contrail_contour[contrail_contour[:, :, 0].argmin()][0]), # leftmost
                 tuple(contrail_contour[contrail_contour[:, :, 0].argmax()][0]), # rightmost
                 tuple(contrail_contour[contrail_contour[:, :, 1].argmin()][0]), # topmost
                 tuple(contrail_contour[contrail_contour[:, :, 1].argmax()][0]), # bottommost
             ]
-
+                        
             dist = np.shape(image)[1] / 2
-            for cand in candidates:
-                cand_dist = np.linalg.norm(np.array(cand) - np.array([center_width, center_height]))
-                if cand_dist < dist:
-                    dist = cand_dist
-                    contrail_end = cand
-            print('end', contrail_end)
+            # for cand in candidates:
+            #     cand_dist = np.linalg.norm(np.array(cand) - np.array([center_width, center_height]))
+            #     if cand_dist < dist:
+            #         dist = cand_dist
+            #         contrail_end = cand
+            # print('end', contrail_end)
 
             contrail_ends.append(contrail_end)
 
@@ -56,22 +94,3 @@ def identify_planes(image):
     else:
         # identify using speed
         return
-
-if __name__ == "__main__":
-    # image_path = "contrails/plane2.jpg"
-    image_path = "filtered_images/cluster_24/2025-04-03T162544933Z.jpeg"
-    # image_path = "contrails/2025-03-19T163904410Z.jpeg"
-    # image_path = "contrails/2025-03-19T164523643Z.jpeg"
-    image = cv2.imread(image_path)
-    # crop out the timestamp
-    image = image[60:, :]
-
-    edges, contrail_ends = identify_planes(image)
-    cv2.imshow("Edges", edges)
-
-    for end in contrail_ends:
-        cv2.circle(image, end, 5, (0, 0, 255), -1)
-
-    cv2.imshow("Result", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
---
+++ main_planes.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+import os
+import datetime
+import identify_planes as p
+
+if __name__ == "__main__":
+    image_path = "filtered_images/cluster_24/2025-04-03T162544933Z.jpeg" # first plane
+    cluster_dir = "filtered_images/cluster_24"
+    # image_path = "filtered_images/cluster_24/2025-04-03T162034887Z.jpeg" # first image
+    image = cv2.imread(image_path)
+    image = image[60:, :]
+    export_path = "marked_planes/cluster_24"
+    os.makedirs(export_path, exist_ok=True)
+
+    # edges, contrail_ends = p.identify_planes(image)
+    # cv2.imshow("Edges", edges)
+    # for e in contrail_ends:
+    #     for c in e:
+    #         cv2.circle(image, c, 5, (0, 0, 255), -1)
+
+    planes_with_time = []
+    plane_pos = -1
+    plane = []
+
+    for root, _, files in os.walk(cluster_dir):
+        for filename in files:
+            # Get time in datetime format
+            base = os.path.splitext(filename)[0]
+
+            date_part, time_part = base.split("T")
+            time_str = time_part.rstrip("Z")
+
+            seconds = time_str[:6]
+            milliseconds = time_str[6:]
+
+            full_time_str = f"{date_part} {seconds}.{milliseconds}"
+
+            dt = datetime.datetime.strptime(full_time_str, "%Y-%m-%d %H%M%S.%f")
+
+            # preprocess image
+            image_path = os.path.join(root, filename)
+            image = cv2.imread(image_path)
+            height, width = image.shape[:2]
+            image = image[60:, :width-30]
+            center = np.array([width / 2, height / 2])
+
+            # get contrails
+            edges, contrail_ends = p.identify_planes(image)
+            if not contrail_ends:
+                continue
+            else:
+                contrail_ends = np.array(contrail_ends)
+                if plane_pos == -1:
+                    contrail_ends = np.reshape(contrail_ends, (np.shape(contrail_ends)[1], np.shape(contrail_ends)[2]))
+                    dist = 99999
+                    for i, cand in enumerate(contrail_ends):
+                        cand_dist = np.linalg.norm(np.array(cand) - center)
+                        if cand_dist < dist:
+                            dist = cand_dist
+                            plane = cand
+                            plane_pos = i
+                    planes_with_time.append((plane, dt))
+                    
+                else:
+                    # if multiple contrails
+                    if np.shape(contrail_ends)[0] > 1:
+
+                        best_dist = 9999
+                        for i, e in enumerate(contrail_ends):
+                            if plane_pos == 0: # left
+                                dist = np.linalg.norm(e[plane_pos][0] - 0)
+                                if dist < best_dist:
+                                    pl = e[plane_pos]
+                                    best_dist = dist
+                            elif plane_pos == 1: # right
+                                dist = np.linalg.norm(e[plane_pos][0] - width)
+                                if dist < best_dist:
+                                    pl = e[plane_pos]
+                                    best_dist = dist
+                            
+                            elif plane_pos == 2: # top
+                                dist = np.linalg.norm(e[plane_pos][1] - 0)
+                                if dist < best_dist:
+                                    pl = e[plane_pos]
+                                    best_dist = dist
+
+                            else: # bottom
+                                dist = np.linalg.norm(e[plane_pos][1] - height)
+                                if dist < best_dist:
+                                    pl = e[plane_pos]
+                                    best_dist = dist
+
+                        contrail_ends = np.reshape(e, (4, 2))
+                        plane = pl
+                    else:
+                        contrail_ends = np.reshape(contrail_ends, (4, 2))
+                        plane = contrail_ends[plane_pos]
+                    planes_with_time.append((plane, dt))
+                    cv2.circle(image, plane, 5, (0, 0, 255), -1)
+                    export_img_path = os.path.join(export_path, filename)
+                    print(f"Exporting to: {export_img_path}")
+                    cv2.imwrite(export_img_path, image)
+
+    # print(planes_with_time)
+    for (p, _) in planes_with_time:
+        cv2.circle(image, p, 5, (0, 0, 255), -1)
+
+    #####TODO: calcualte pose of planes and compare to ground truth (in .json files)########
+                    
+    cv2.imshow("Result", image)
+    export_img_path = os.path.join(export_path, "result.jpeg")
+    cv2.imwrite(export_img_path, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
